2fbfa3ef76a693ff395de86d50fc0d23c2578854
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-04-22 15:43:41 +0200

    f

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -72,7 +72,6 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
